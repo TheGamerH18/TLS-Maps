fd0b08be322892d2c1ef9514b01782e6db56a772
TheGamerH18 <44879628+TheGamerH18@users.noreply.github.com>
2021-06-03 12:21:10 +0200

    Revert "Cleanup and added Comments"
    This reverts commit 79ae659ae0710d9216fab7f6d78f4caeb4fa2dda.

--- app/src/main/java/main/tls_maps/Notifications/Notification.java
+++ app/src/main/java/main/tls_maps/Notifications/Notification.java
@@ -12,57 +12,41 @@ import main.tls_maps.R;
 
 public class Notification {
 
-    /**
-     * This Class is to make a Notification much more easier
-     */
-
-    // Static ID to get a unique ID
+    String msg, title;
     static int ID = 0;
     int notificationID;
     NotificationManagerCompat notificationManager;
     NotificationCompat.Builder builder;
 
     public Notification(String msg, String title, Context context) {
-
-        // Check if the Android Version is below 26
-        // Only available in SDK 26 and Heigher
         if ( Build.VERSION.SDK_INT >= 26 ) {
-            CharSequence name = "TLS-Maps: Notifications";
-            String description = "Channel for Notification";
-            // Set the Importance to the Channel
+            CharSequence name = "studentChannel";
+            String description = "Channel for notification";
             int importance = NotificationManager.IMPORTANCE_DEFAULT;
-
-            // Create the Channel
             NotificationChannel channel = new NotificationChannel(""+ID, name, importance);
             channel.setDescription(description);
+
             NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
             notificationManager.createNotificationChannel(channel);
         }
         notificationManager = NotificationManagerCompat.from(context);
-        // Create the Title
         title = "TLS-Maps " + title;
-
-        // Create the Notification
         builder = new NotificationCompat.Builder(context, ""+ID)
                 .setSmallIcon(R.mipmap.ic_launcher_round)
                 .setContentTitle(title)
                 .setContentText(msg)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
-
-        // Save the ID for this Notification
         this.notificationID = ID;
         ++ID;
     }
 
     public void Notify () {
-        // Display the Notification
         if(notificationManager == null)
             return;
         notificationManager.notify(this.notificationID, this.builder.build());
     }
 
     public android.app.Notification getNotification() {
-        // Get the Notification
         if(this.builder == null)
             return new android.app.Notification();
         return this.builder.build();
--- app/src/main/java/main/tls_maps/Notifications/NotificationTimer.java
+++ app/src/main/java/main/tls_maps/Notifications/NotificationTimer.java
@@ -8,22 +8,17 @@ import androidx.core.app.NotificationManagerCompat;
 
 public class NotificationTimer extends BroadcastReceiver {
 
-    /**
-     * This Class is for the Time Based Notification
-     */
+
+    Notification notification;
 
     public NotificationTimer () {
-        // Needed for the init
         super();
     };
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        // get the notification from the Intent
         android.app.Notification notification = intent.getParcelableExtra( "Notification" );
-        // get the ID from the Notification
-        int id = Integer.parseInt(intent.getParcelableExtra( "ID" ));
         NotificationManagerCompat NM = NotificationManagerCompat.from(context);
-        NM.notify(id, notification);
+        NM.notify(1 ,notification);
     }
 }
