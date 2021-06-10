42d3ef38f5750cf963e40ece596fba24b5ef47bc
TheGamerH18 <44879628+TheGamerH18@users.noreply.github.com>
2021-06-10 14:31:41 +0200

    Optimize Imports

--- app/src/main/java/main/tls_maps/MainActivity.java
+++ app/src/main/java/main/tls_maps/MainActivity.java
@@ -1,10 +1,6 @@
 package main.tls_maps;
 
 import android.app.Activity;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
